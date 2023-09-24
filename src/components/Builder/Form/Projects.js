17d218ee95b640f880b420d25b121ce0fff0e023
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-09-24 23:13:17 +0530

    Add multiple projects

--- src/components/Builder/Form/Projects.js
+++ src/components/Builder/Form/Projects.js
@@ -1,8 +1,15 @@
 import { useGlobalAppContext } from "@/context/context";
+import { findIndex } from "@/utils/custom";
 import React, { useState } from "react";
 
-const Projects = ({id}) => {
-  const { fetchResumedata, currentData,updateResumeRecord,activeTab, setActiveTab } = useGlobalAppContext();
+const Projects = ({ id }) => {
+  const {
+    fetchResumedata,
+    currentData,
+    updateResumeRecord,
+    activeTab,
+    setActiveTab,
+  } = useGlobalAppContext();
 
   const [formData, setFormData] = useState({
     title: "",
@@ -46,142 +53,162 @@ const Projects = ({id}) => {
     console.log(projects);
   };
 
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    const editdata = findIndex(projects, index);
+    setFormData(editdata);
+  };
+  const handleSaveEdit = () => {
+    if (editIndex !== -1) {
+      const updatedData = [...projects];
+      updatedData[editIndex] = formData;
+      setProjects(updatedData);
+      setEditIndex(-1);
+      setFormData({
+        title: "",
+        projectTitle: "",
+        overview: "",
+        deployedUrl: "",
+        repositoryUrl: "",
+        description: "",
+      });
+    }
+  };
   return (
     <div className="w-full max-w-screen-xl mx-auto">
-      <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleSubmit}
-      >
-        <h2 className="text-2xl font-bold mb-4">Projects</h2>
-        {projects.map((project, index) => (
-          <div key={index} className="mb-6 border-b-2 pb-4">
-            <div className="flex justify-between items-center my-5">
-              <h3 className="text-lg font-bold mb-2">Project {index + 1}</h3>{" "}
-              <button
-                type="button"
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                onClick={() => handleRemove(index)}
-              >
-                Remove
-              </button>
-            </div>
-            <div className="mb-4">
-              <div className="w-full max-w-screen-xl mx-auto">
-                <form
-                  onSubmit={handleSubmit}
-                  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                >
-                  <div className="grid grid-cols-2 gap-4">
-                    {/* Full-width field */}
-                   
-
-                    {/* 50% width fields */}
-                    <div className="mb-4">
-                      <label
-                        className="block mb-2 text-gray-600"
-                        htmlFor="projectTitle"
-                      >
-                        Project Title
-                      </label>
-                      <input
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                        type="text"
-                        id="projectTitle"
-                        name="projectTitle"
-                        value={formData.projectTitle}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <label
-                        className="block mb-2 text-gray-600"
-                        htmlFor="overview"
-                      >
-                        Overview
-                      </label>
-                      <input
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                        type="text"
-                        id="overview"
-                        name="overview"
-                        value={formData.overview}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <label
-                        className="block mb-2 text-gray-600"
-                        htmlFor="deployedUrl"
-                      >
-                        Deployed URL
-                      </label>
-                      <input
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                        type="text"
-                        id="deployedUrl"
-                        name="deployedUrl"
-                        value={formData.deployedUrl}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <label
-                        className="block mb-2 text-gray-600"
-                        htmlFor="repositoryUrl"
-                      >
-                        Repository URL
-                      </label>
-                      <input
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                        type="text"
-                        id="repositoryUrl"
-                        name="repositoryUrl"
-                        value={formData.repositoryUrl}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div className="col-span-2 mb-4">
-                      <label
-                        className="block mb-2 text-gray-600"
-                        htmlFor="description"
-                      >
-                        Project Description
-                      </label>
-                      <textarea
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                        id="description"
-                        name="description"
-                        value={formData.description}
-                        onChange={handleChange}
-                      ></textarea>
-                    </div>
+      <h2 className="text-2xl font-bold mb-4">Projects</h2>
+      <div className="mb-6 border-b-2 pb-4">
+        <div className="mb-4">
+          <div className="w-full max-w-screen-xl mx-auto">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="mb-4">
+                    <label
+                      className="block mb-2 text-gray-600"
+                      htmlFor="projectTitle"
+                    >
+                      Project Title
+                    </label>
+                    <input
+                      className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                      type="text"
+                      id="projectTitle"
+                      name="projectTitle"
+                      value={formData.projectTitle}
+                      onChange={handleChange}
+                    />
                   </div>
-                </form>
+                  <div className="mb-4">
+                    <label
+                      className="block mb-2 text-gray-600"
+                      htmlFor="overview"
+                    >
+                      Overview
+                    </label>
+                    <input
+                      className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                      type="text"
+                      id="overview"
+                      name="overview"
+                      value={formData.overview}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="mb-4">
+                    <label
+                      className="block mb-2 text-gray-600"
+                      htmlFor="deployedUrl"
+                    >
+                      Deployed URL
+                    </label>
+                    <input
+                      className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                      type="text"
+                      id="deployedUrl"
+                      name="deployedUrl"
+                      value={formData.deployedUrl}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block mb-2 text-gray-600"
+                      htmlFor="repositoryUrl"
+                    >
+                      Repository URL
+                    </label>
+                    <input
+                      className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                      type="text"
+                      id="repositoryUrl"
+                      name="repositoryUrl"
+                      value={formData.repositoryUrl}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 gap-4">
+                  <div className="col-span-2 mb-4">
+                    <label
+                      className="block mb-2 text-gray-600"
+                      htmlFor="description"
+                    >
+                      Project Description
+                    </label>
+                    <textarea
+                      className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                      id="description"
+                      name="description"
+                      value={formData.description}
+                      onChange={handleChange}
+                    ></textarea>
+                  </div>
+                </div>
               </div>
+              <div className="mb-6">
+                {editIndex === -1 ? (
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={handleAdd}
+                  >
+                    Add Skill
+                  </button>
+                ) : (
+                  <button
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={handleSaveEdit}
+                  >
+                    Update Skill
+                  </button>
+                )}
+              </div>
+            </form>
+            <div className="flex flex-wrap">
+              {projects.map((project, index) => (
+                <ProjectCard
+                  key={index}
+                  {...project}
+                  onEdit={() => handleEdit(index)}
+                  onDelete={() => handleRemove(index)}
+                />
+              ))}
             </div>
           </div>
-        ))}
-        <div className="mb-6">
-          {/* <h3 className="text-lg font-bold mb-2">Add Project</h3> */}
-          <div className="mb-6">
-            {/* Input fields for the currentExperience */}
-          </div>
-          <button
-            type="button"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            onClick={handleAdd}
-          >
-            Add Project
-          </button>
         </div>
+
         <div className="flex items-center justify-between">
-        <button
+          <button
             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
             onClick={() => {
-              setProjects([
-              
-              ]);
+              setProjects([]);
               setFormData({
                 title: "",
                 projectTitle: "",
@@ -200,11 +227,54 @@ const Projects = ({id}) => {
           >
             Save
           </button>
-         
         </div>
-      </form>
+      </div>
     </div>
   );
 };
 
 export default Projects;
+
+const ProjectCard = ({
+  title,
+  projectTitle,
+  overview,
+  deployedUrl,
+  repositoryUrl,
+  description,
+  onEdit,
+  onDelete,
+}) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden m-4 p-4">
+      <div className="mb-4">
+        <h2 className="text-xl font-semibold">{title}</h2>
+        <p className="text-gray-600">{projectTitle}</p>
+        <p className="text-gray-600">{overview}</p>
+        <p className="text-gray-600">
+          Deployed URL: <a href={deployedUrl}>{deployedUrl}</a>
+        </p>
+        <p className="text-gray-600">
+          Repository URL: <a href={repositoryUrl}>{repositoryUrl}</a>
+        </p>
+        <p className="text-gray-600">{description}</p>
+      </div>
+      <div className="mt-auto">
+        <div className="flex justify-between">
+          <button
+            onClick={() => onEdit()}
+            className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => onDelete()}
+            className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105 ml-2"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
