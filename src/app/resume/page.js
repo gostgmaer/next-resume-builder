2993292f95178f1ab7da102387bf73569347d43e
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-09-24 23:01:19 +0530

    changed for error

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,21 +1,8 @@
-"use client"
-import Skill from '@/components/expriment/Skills';
-import Head from 'next/head';
-import React, { useState } from 'react'
+import React from 'react'
 
 const page = () => {
-  const [skills, setSkills] = useState([]);
   return (
-    <div>
-    <Head>
-      <title>Resume Skills</title>
-    </Head>
-
-    <main className="p-4">
-      <h1 className="text-3xl font-semibold mb-4">Resume Skills</h1>
-      <Skill skills={skills} setSkills={setSkills} />
-    </main>
-  </div>
+    <div>page</div>
   )
 }
 
